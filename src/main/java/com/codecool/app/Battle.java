ba17e6c08079d9ea2668dff28747d58d5b036612
Grelory <gmpwm99@gmail.com>
2020-04-22 10:24:22 +0200

    first step to create multiplayer mode

--- src/main/java/com/codecool/app/Battle.java
+++ src/main/java/com/codecool/app/Battle.java
@@ -17,10 +17,12 @@ public class Battle {
         Card player1Card = player1.throwCard(0);
         Card player2Card = player2.throwCard(0);
 
-        StatsType statsType = display.statisticsSelection(player1Card.getCardImage(player1.getAmountOfCards()), "asdfasdf");
+        StatsType statsType = player1.getStatsType(player1Card.getCardImage(player1.getAmountOfCards()));
         display.printCardsHand(player2Card.getCardImage(player2.getAmountOfCards()), player2.getName());
         display.pressEnterToContinue();
-       
+
+        display.battleScreen(player1.getName(), player2.getName(), player1Card.getCardImage(1), player2Card.getCardImage(1));
+        display.pressEnterToContinue();
         // if(player1Card.compareCard(player2Card, chosenStat)) {
         //     player1.addCard(player1Card);
         //     player1.addCard(player2Card);
